b306a0c40708d09ffe909532b2a3a407a4a1b7da
Keila Andrea Flores Garcete <keilaflores889@gmail.com>
2024-07-22 19:26:31 -0400

    actualizado

--- js/app.js
+++ js/app.js
@@ -6,8 +6,23 @@ HTML, por ultimo javascript
 
 document
 .addEventListener("DOMContentLoaded", function(){
-alert(Holis);
+//esta es zona segura
+ //console.log(document.getElementById("txtNombre").value);
+ //console.log(document.getElementById("txtApellido").value);
+
 });
+
+//añadiendo evento clic al boton mostrar
+const btnMostrar = document.getElementById("btnMostrar");
+btnMostrar.addEventListener("click", function(){
+    //console.log("haciendo clic");
+    //alert("hice clic");
+    //recuperando info del formulario
+    const nombres = document.getElementById("txtNombre").value;
+    const apellido = document.getElementById("txtApellido").value;
+    const cedula = document.getElementById("txtCedula").value;
+    console.log('nombre' + nombres + ', apellidos' + apellido +  ', cedula' + cedula);
+})
 
 
 function hola(nombre, apellido){
